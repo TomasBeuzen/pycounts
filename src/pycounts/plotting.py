07052649021b7422c282727f4f4c11df910a3a0b
Tomas Beuzen <tomas.beuzen@stat.ubc.ca>
2021-09-04 15:01:26 +1000

    feat: add plotting module

---
+++ src/pycounts/plotting.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+
+def plot_words(word_counts, n=10):
+    """Plot a bar chart of word counts."""
+    top_n_words = word_counts.most_common(n)
+    word, count = zip(*top_n_words)
+    fig = plt.bar(range(n), count)
+    plt.xticks(range(n), labels=word, rotation=45)
+    plt.xlabel("Word")
+    plt.ylabel("Count")
+    return fig
