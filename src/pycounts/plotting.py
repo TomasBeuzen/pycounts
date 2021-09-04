27488910fe885311e91373d7b562a693d0070992
Tomas Beuzen <tomas.beuzen@stat.ubc.ca>
2021-09-04 15:46:52 +1000

    docs: created docstrings for package functions

--- src/pycounts/plotting.py
+++ src/pycounts/plotting.py
@@ -1,7 +1,27 @@
 import matplotlib.pyplot as plt
 
 def plot_words(word_counts, n=10):
-    """Plot a bar chart of word counts."""
+    """Plot a bar chart of word counts.
+    
+    Parameters
+    ----------
+    word_counts : collections.Counter
+        Counter object of word counts.
+    n : int, optional
+        Plot the top n words. By default, 10.
+
+    Returns
+    -------
+    matplotlib.container.BarContainer
+        Bar chart of word counts.
+
+    Examples
+    --------
+    >>> from pycounts.pycounts import count_words
+    >>> from pycounts.plotting import plot_words
+    >>> counts = count_words("text.txt")
+    >>> plot_words(counts)
+    """
     top_n_words = word_counts.most_common(n)
     word, count = zip(*top_n_words)
     fig = plt.bar(range(n), count)
